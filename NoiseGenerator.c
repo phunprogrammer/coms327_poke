01ed2212c9fc2c3a2a76771e9957e38e43835b81
phun <pnguyen2@iastate.edu>
2024-01-24 16:32:33 -0600

    NoiseGen 1st iteration

---
+++ NoiseGenerator.c
@@ -0,0 +1,40 @@
+#include "PerlinNoise.h"
+
+#define HEIGHT 21
+#define WIDTH 80
+#define WAVENUM 5
+
+typedef struct Wave {
+    float seed;
+    float frequency;
+    float amplitude;
+} wave_t;
+
+typedef struct Offset {
+    float x;
+    float y;
+} offset_t;
+
+float* Generate (float scale, wave_t waves[WAVENUM], offset_t offset) {
+    float noiseMap[WIDTH][HEIGHT] = { 0 };
+
+    int i,j,k;
+
+    for (i = 0; i < WIDTH; i++) {
+        for (j = 0; j < HEIGHT; j++) {
+            float posX = (float)i * scale + offset.x;
+            float posY = (float)j * scale + offset.y;
+
+            float normalization = 0.0f;
+
+            for (k = 0; k < WAVENUM; k++) {
+                noiseMap[i][j] += waves[k].amplitude * noise2(posX * waves[k].frequency + waves[k].seed, posY * waves[k].frequency + waves[k].seed);
+                normalization += waves[k].amplitude;
+            }
+
+            noiseMap[i][j] /= normalization;
+        }
+    }
+
+    return noiseMap;
+}   

4b9d931721cc0b233fd392c88ff8c1efea22bc3e
phun <phun5701@gmail.com>
2024-01-30 19:26:54 -0600

    Added Priority Queue

---
+++ PQueue.c
@@ -0,0 +1,66 @@
+#include "PQueue.h"
+#include <stdlib.h>
+
+int pq_init(pqueue_t *queue) {
+    queue->size = 0;
+    return 0;
+}
+
+int pq_destroy(pqueue_t *queue) {
+    free(queue->array);
+    return 0;
+}
+
+int pq_enqueue(pqueue_t *queue, void *data, int priority) {
+    pqnode_t node = { .data = data, .priority = priority };
+
+    queue->array[pq_size(queue)] = node;
+
+    for(int i = pq_size(queue) - 1; i >= 0; i--) {
+        if(priority >= (queue->array[i]).priority)
+            break;
+
+        pq_swap(queue, i + 1, i);
+    }
+
+    queue->size++;
+
+    return 0;
+}
+
+int pq_swap(pqueue_t *queue, int a, int b) {
+    if(pq_isEmpty(queue) || a > pq_size(queue) || b > pq_size(queue) || a < 0 || b < 0 || a == b) 
+        return 1;
+
+    pqnode_t tempNode;
+    tempNode = queue->array[a];
+    queue->array[a] = queue->array[b];
+    queue->array[b] = tempNode;
+
+    return 0;
+}
+
+int pq_dequeue(pqueue_t *queue, void **data) {
+    if(pq_size(queue) == 0) return 1;
+
+    *data = queue->array[pq_size(queue) - 1].data;
+    queue->size--;
+
+    return 0;
+}
+
+int pq_peek(pqueue_t *queue, void **data) {
+    if(pq_size(queue) == 0) return 1;
+
+    *data = queue->array[pq_size(queue) - 1].data;
+
+    return 0;
+}
+
+int pq_size(pqueue_t *queue) {
+    return queue->size;
+}
+
+int pq_isEmpty(pqueue_t *queue) {
+    return queue->size == 0;
+}
